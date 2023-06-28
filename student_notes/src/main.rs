2e8bdceadc9f52339d7cd5f4962c77daf1a22a58
Noah Medoff <noahmedoff@gmail.com>
2023-06-28 10:28:42 -0700

    meowmeow just doing borrow and move functions

--- student_notes/src/main.rs
+++ student_notes/src/main.rs
@@ -4,9 +4,11 @@
 mod data_types;
 mod examples;
 mod rust_basics;
+mod rust_nuances;
 // use examples::lemonade_stand or use examples::*;
 
 fn main() {
     // rust_basics::main();
     // data_types::main();
+    rust_nuances::main();
 }
--- student_notes/src/rust_nuances.rs
+++ student_notes/src/rust_nuances.rs
@@ -1,1 +1,25 @@
 // ownership, borrowing, lifetimes, references, borrow checker, and pointers
+fn bf1(s: &String) {
+    println!("{s}")
+}
+
+fn bf2(s: String) {
+    println!("{s}")
+}
+
+fn borrow_func() {
+    let string1: String = "Hello".to_string();
+    bf1(&string1);
+    println!("================");
+    bf2(string1.clone());
+}
+
+fn mov_func() {
+    let str1: String = "hello".to_string();
+    let str2: String = str1.clone();
+}
+
+pub fn main() {
+    borrow_func();
+    mov_func();
+}
